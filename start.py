8859323e1d62d2609447f6656e5ae5ea50adf2e2
Lolenseu <98665691+lolenseu@users.noreply.github.com>
2022-03-01 01:27:45 +0800

    Update start.py

--- start.py
+++ start.py
@@ -15,17 +15,13 @@ num2 = float(input("Type a Second Number: "))
 def main():
     #Computations
     if solution == Addition:
-        print("Addition of", num1, "and", num2, "is")
-        print(num1 + num2)
+        print("Addition of", num1, "and", num2, "is ", num1 + num2)
     elif solution == Subtraction:
-        print("Subtraction of", num1, "and", num2, "is")
-        print(num1 - num2)
+        print("Subtraction of", num1, "and", num2, "is ", num1 - num2)
     elif solution == Multiplication:
-        print("Multiplication of", num1, "and", num2, "is")
-        print(num1 * num2)
+        print("Multiplication of", num1, "and", num2, "is ", num1 * num2)
     elif solution == Divition:
-        print("Divition of", num1, "and", num2, "is")
-        print(num1 / num2)
+        print("Divition of", num1, "and", num2, "is ", num1 / num2)
     #Error
     elif [solution != Addition, solution != Subtraction, solution != Multiplication, solution != Divition] or [num1 and num2 != float() or int()]:
         print("Math Error")
