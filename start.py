42f3d8b033e496b97aa631883d781bd6c56fb11f
Lolenseu <98665691+lolenseu@users.noreply.github.com>
2022-03-05 21:44:08 +0800

    Update start.py

--- start.py
+++ start.py
@@ -3,11 +3,14 @@ Addition = "add"
 Subtraction = "sub"
 Multiplication = "mul"
 Divition = "div"
+squareRoot = "sqr"
 
 def main():
     #Info
     print("Type (add) if you use Addition, Type (sub) if you use Subtraction, Type (mul) if you use Multiplication and Type (div) if you use Divition")
-
+    print("Type (sqr) if you use Square_Root")
+    print("Example: Number (add/sub/mul/div/sqr) Second Number")
+    
     #Fuctions
     solution = str(input("Solution here: "))
     num1 = float(input("Type a Number: "))
@@ -22,6 +25,8 @@ def main():
         print("Multiplication of", num1, "and", num2, "is", num1 * num2)
     elif solution == Divition:
         print("Divition of", num1, "and", num2, "is", num1 / num2)
+    elif solution == squareRoot:
+	    print("Square Root of", num1, "and", num2, "is", num1 ** num2)
     #Error
     elif [solution != Addition, solution != Subtraction, solution != Multiplication, solution != Divition] or [num1 and num2 != float() or int()]:
         print("Math Error")
@@ -30,4 +35,6 @@ def main():
         main()
     else:
         exit()
+
+
 main()
