2acada5a3f1fe58a31d28c12b72abc102819c965
lolenseu <98665691+lolenseu@users.noreply.github.com>
2022-04-13 00:42:03 +0800

    Add files via upload

--- start.py
+++ start.py
@@ -1,3 +1,7 @@
+import os
+
+os.system('clear')
+
 #Identifications
 Addition = "add"
 Subtraction = "sub"
@@ -5,16 +9,53 @@ Multiplication = "mul"
 Divition = "div"
 squareRoot = "sqr"
 
-def main():
-    #Info
-    print("Type (add) if you use Addition, Type (sub) if you use Subtraction, Type (mul) if you use Multiplication and Type (div) if you use Divition")
-    print("Type (sqr) if you use Square_Root")
-    print("Example: Number (add/sub/mul/div/sqr) Second Number")
-    
+#Info
+print("Type (add) for Addition, Type (sub) for Subtraction, Type (mul) for Multiplication, and Type (div) for Divition")
+print("Type (sqr) for Square_Root")
+print("Example: Number (add/sub/mul/div/sqr) Second Number")
+
+reload = True
+while reload:
     #Fuctions
-    solution = str(input("Solution here: ")).lower()
-    num1 = float(input("Type a Number: "))
-    num2 = float(input("Type a Second Number: "))
+    def method():
+        global solution
+        sub_solution = input("Solution here: ").lower()
+        if sub_solution == 'add' or sub_solution == 'sub' or sub_solution == 'mul' or sub_solution == 'div' or sub_solution == 'sqr':
+            solution = str(sub_solution)
+            pass
+        elif sub_solution != str():
+            print("Please Choose on the Solution!")
+            method()
+        else:
+            print("Please Choose on the Solution!")
+            method()
+
+
+    method()
+    no1 = False
+    while not no1:
+        try:
+            num1 = float(input("Type a Number: "))
+            if num1 == str():
+                no1 = False
+            else:
+                no1 = True
+        except:
+            print("Please Type a valid Number!")
+            continue
+
+    no2 = False
+    while not no2:
+        try:
+            num2 = float(input("Type a Second Number: "))
+            if num2 == str():
+                no2 = False
+            else:
+                no2 = True
+        except:
+            print("Please Type a valid Number!")
+            continue
+    
 
     #Computations
     if solution == Addition:
@@ -27,14 +68,3 @@ def main():
         print("Divition of", num1, "and", num2, "is", num1 / num2)
     elif solution == squareRoot:
 	    print("Square Root of", num1, "and", num2, "is", num1 ** num2)
-    #Error
-    elif [solution != Addition, solution != Subtraction, solution != Multiplication, solution != Divition] or [num1 and num2 != float() or int()]:
-        print("Math Error")
-    win = str(input("Press Enter to Reload"))  
-    if win is str(): 
-        main()
-    else:
-        exit()
-
-
-main()
